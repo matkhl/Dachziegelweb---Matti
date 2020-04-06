accefc395b0e7ae20b87b370b1889f582c614473
matkhl <matkhl9323@gmail.com>
2020-04-06 12:40:38 +0200

    initalize project

---
+++ impressum.php
@@ -0,0 +1,128 @@
+<!--IMPRESSUM-->
+<!DOCTYPE html>
+
+<html lang="de">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Impressum | dzw</title>
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,400i,500" rel="stylesheet">
+    <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/entypo.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+  </head>
+
+  <body class="bg-light">
+
+    <?php include("./imports/header.html"); ?>
+
+    <div class="main-container">
+      <section class="bg-white space-sm">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h1>Dachziegelweb</h1>
+              <span class="lead">In Sekunden den passenden Ziegel finden.</span>
+              <a href="#" class="btn btn-success">Lösungen</a>
+            </div>
+            <!--end of col-->
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <!--end of section-->
+      <section>
+        <div class="container">
+          <div class="justify-content-between">
+            <article>
+              <h1>Impressum</h1>
+
+              <h2>Angaben gemäß § 5 TMG</h2>
+              <p data-aos="fade-up">WZP dachziegelweb UG (haftungsbeschränkt)<br />
+              Neustädtische Wassertorstr. 21<br />
+              14776 Brandenburg an der Havel</p>
+
+              <p data-aos="fade-up">Handelsregister: HRB 28485P<br />
+              Registergericht: Amtsgericht Potsdam</p>
+
+              <p data-aos="fade-up"><strong>Vertreten durch:</strong><br />
+              Angela Müller</p>
+
+              <h2>Kontakt</h2>
+              <p data-aos="fade-up">Telefon: +49 (0) 3381 / 702914<br />
+              E-Mail: info@dachziegelweb.de</p>
+
+              <h2>Umsatzsteuer-ID</h2>
+              <p data-aos="fade-up">Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:<br />
+              DE815603216</p>
+
+              <h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h2>
+              <p data-aos="fade-up">Angela Müller</p>
+
+              <h2>Streitschlichtung</h2>
+              <p data-aos="fade-up">Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr</a>.<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
+
+              <p data-aos="fade-up">Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+              <h3>Haftung für Inhalte</h3>
+              <p data-aos="fade-up">Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
+              <p data-aos="fade-up">Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+
+              <h3>Haftung für Links</h3>
+              <p data-aos="fade-up">Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.<p>
+              <p data-aos="fade-up">Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+
+              <h3>Urheberrecht</h3>
+              <p data-aos="fade-up">Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
+              <p data-aos="fade-up">Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+            </article>
+            <hr>
+          </div>
+        </div>
+        <!--end of container-->
+      </section>
+
+      <?php include("./imports/footer.html"); ?>
+
+    </div>
+
+    <!-- Required vendor scripts (Do not remove) -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+
+    <!-- Optional Vendor Scripts (Remove the plugin script here and comment initializer script out of index.js if site does not use that feature) -->
+
+    <!-- AOS (Animate On Scroll - animates elements into view while scrolling down) -->
+    <script type="text/javascript" src="js/aos.js"></script>
+    <!-- Flatpickr (calendar/date/time picker UI) -->
+    <script type="text/javascript" src="js/flatpickr.min.js"></script>
+    <!-- Flickity (handles touch enabled carousels and sliders) -->
+    <script type="text/javascript" src="js/flickity.pkgd.min.js"></script>
+    <!-- jarallax (parallax effect and video backgrounds) -->
+    <script type="text/javascript" src="js/jarallax.min.js"></script>
+    <script type="text/javascript" src="js/jarallax-video.min.js"></script>
+    <script type="text/javascript" src="js/jarallax-element.min.js"></script>
+    <!-- ScrollMonitor (manages events for elements scrolling in and out of view) -->
+    <script type="text/javascript" src="js/scrollMonitor.js"></script>
+    <!-- jQuery smartWizard facilitates steppable wizard content -->
+    <script type="text/javascript" src="js/jquery.smartWizard.min.js"></script>
+    <!-- Smooth scroll (animation to links in-page)-->
+    <script type="text/javascript" src="js/smooth-scroll.polyfills.min.js"></script>
+    <!-- Prism (displays formatted code boxes) -->
+    <script type="text/javascript" src="js/prism.js"></script>
+    <script type="text/javascript" src="js/zoom.min.js"></script>
+
+    <!-- Required theme scripts (Do not remove) -->
+    <script type="text/javascript" src="js/theme.js"></script>
+
+    <script type="text/javascript">
+      $(document).ready(function(){
+        $("#header").load("imports/header.html");
+      });
+    </script>
+
+  </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,184 @@
+<!--STARTSEITE-->
+<!DOCTYPE html>
+
+<html lang="de">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Hersteller und Lieferanten | dzw</title>
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,400i,500" rel="stylesheet">
+    <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/entypo.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+  </head>
+
+  <body class="bg-light">
+
+    <?php include("./imports/header.html"); ?>
+
+    <div class="main-container">
+      <div class="container" style="margin-bottom: 150px;">
+
+        <div class="text-center pb-3 mt-5">
+          <h1>Hersteller und Lieferanten</h1>
+          <br>
+          <p>Sie möchten sich Dachsteine oder Dachziegel von einem bestimmten Hersteller ansehen?<br>
+          Klicken Sie einfach den Herstellerlink an, wählen dann das Produkt aus, ob Dachstein oder Dachziegel.<br>
+          Sie finden nun alle im Lieferprogramm enthaltenen Dachsteine und Dachziegel.</p>
+        </div>
+
+        <div class="row text-center mt-5">
+          <div class="col-md-4">
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Alphons Meyer GmbH & Co. KG</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\AlphMeyerGmbH_Logo.jpg" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Benders Deutschland GmbH</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\benders_deutschland_Logo.png" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Braas GmbH</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\braas_Logo.jpg" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Creaton GmbH</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\Creaton_GmbH_Logo.png" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Dachkeramik Meyer Holsen GmbH</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\Dachkeramik_Meyer_Holsen_GmbH_Logo.png" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-4">
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Dachziegelwerke Nelskamp GmbH</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\Dachziegelwerke_Nelskamp_GmbH_Logo.png" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 px-4 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Erlus AG</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\Erlus_AG_Logo.png" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Gebr. Laumans GmbH & Co. KG</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\Gebr_Laumans_GmbH_Co_KG_Logo.png" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Jacobi Tonwerke GmbH</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\Jacobi_Tonwerke_GmbH_Logo.png" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header px-0 bg-white font-weight-bold" style="border: none;">KDW-Klinkerdachziegelwerk GmbH & Co. KG</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\ABC-Keramik_A_Berentelg_Co_Logo.jpg" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-4">
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">La Esceramica Vertriebs GmbH</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\La_Esceramica_Vertrieb_GmbH_Logo.png" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Rimini Baustoffe GmbH</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\Rimini_Baustoffe_GmbH_Logo.jpg" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Röben Tonbaustoffe GmbH</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\Roeben_Tonbaustoffe_GmbH_Logo.jpg" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Walther Dachziegel GmbH</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\Walther_Dachziegel_GmbH_Logo.png" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+            <div class="card mb-5 aos-init" data-aos="fade-up" style="height: 15%;">
+              <div class="card-header bg-white font-weight-bold" style="border: none;">Wienerberger GmbH - Koramic</div>
+              <div class="card-body px-5">
+                <img src="res\img\Hersteller_Lieferanten\Wienerberger_GmbH_Koramic_Logo.png" alt="" style="max-width: 100%; max-height: 100%;">
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <?php include("./imports/footer.html"); ?>
+
+    </div>
+
+    <!-- Required vendor scripts (Do not remove) -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+
+    <!-- Optional Vendor Scripts (Remove the plugin script here and comment initializer script out of index.js if site does not use that feature) -->
+
+    <!-- AOS (Animate On Scroll - animates elements into view while scrolling down) -->
+    <script type="text/javascript" src="js/aos.js"></script>
+    <!-- Flatpickr (calendar/date/time picker UI) -->
+    <script type="text/javascript" src="js/flatpickr.min.js"></script>
+    <!-- Flickity (handles touch enabled carousels and sliders) -->
+    <script type="text/javascript" src="js/flickity.pkgd.min.js"></script>
+    <!-- jarallax (parallax effect and video backgrounds) -->
+    <script type="text/javascript" src="js/jarallax.min.js"></script>
+    <script type="text/javascript" src="js/jarallax-video.min.js"></script>
+    <script type="text/javascript" src="js/jarallax-element.min.js"></script>
+    <!-- ScrollMonitor (manages events for elements scrolling in and out of view) -->
+    <script type="text/javascript" src="js/scrollMonitor.js"></script>
+    <!-- jQuery smartWizard facilitates steppable wizard content -->
+    <script type="text/javascript" src="js/jquery.smartWizard.min.js"></script>
+    <!-- Smooth scroll (animation to links in-page)-->
+    <script type="text/javascript" src="js/smooth-scroll.polyfills.min.js"></script>
+    <!-- Prism (displays formatted code boxes) -->
+    <script type="text/javascript" src="js/prism.js"></script>
+    <script type="text/javascript" src="js/zoom.min.js"></script>
+
+    <!-- Required theme scripts (Do not remove) -->
+    <script type="text/javascript" src="js/theme.js"></script>
+
+  </body>
+
+</html>
