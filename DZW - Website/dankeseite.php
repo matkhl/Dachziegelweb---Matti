084a57003ba94dfe463b1982dfdb394bc8d3e5c9
matkhl <matkhl9323@gmail.com>
2020-04-08 00:00:12 +0200

    Dankeseite hinzugefügt

---
+++ DZW - Website/dankeseite.php
@@ -0,0 +1,66 @@
+<!--HERSTELLER UND LIEFERANTEN-->
+<!DOCTYPE html>
+
+<html lang="de">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Hersteller und Lieferanten | dzw</title>
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,400i,500" rel="stylesheet">
+    <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/entypo.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+  </head>
+
+  <body class="bg-light">
+
+    <?php include("./imports/header.html"); ?>
+
+    <div class="main-container">
+      <div class="container" style="margin-bottom: 150px;">
+        <div class="text-center pb-3 mt-5">
+          <p class="text-muted">Jetzt mit der Profi-Suche Starten.</p>
+          <h1>Vielen Dank für Ihre Bestellung</h1>
+          <p>Als nächstes erhalten Sie eine Bestätigungsmail.<br>
+          Die Abbuchung erfolgt durch Digistore24.</p>
+          <button class="btn btn-success" type="button" name="button">Zum Login</button>
+        </div>
+      </div>
+
+      <?php include("./imports/footer.html"); ?>
+
+    </div>
+
+    <!-- Required vendor scripts (Do not remove) -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+
+    <!-- Optional Vendor Scripts (Remove the plugin script here and comment initializer script out of index.js if site does not use that feature) -->
+
+    <!-- AOS (Animate On Scroll - animates elements into view while scrolling down) -->
+    <script type="text/javascript" src="js/aos.js"></script>
+    <!-- Flatpickr (calendar/date/time picker UI) -->
+    <script type="text/javascript" src="js/flatpickr.min.js"></script>
+    <!-- Flickity (handles touch enabled carousels and sliders) -->
+    <script type="text/javascript" src="js/flickity.pkgd.min.js"></script>
+    <!-- jarallax (parallax effect and video backgrounds) -->
+    <script type="text/javascript" src="js/jarallax.min.js"></script>
+    <script type="text/javascript" src="js/jarallax-video.min.js"></script>
+    <script type="text/javascript" src="js/jarallax-element.min.js"></script>
+    <!-- ScrollMonitor (manages events for elements scrolling in and out of view) -->
+    <script type="text/javascript" src="js/scrollMonitor.js"></script>
+    <!-- jQuery smartWizard facilitates steppable wizard content -->
+    <script type="text/javascript" src="js/jquery.smartWizard.min.js"></script>
+    <!-- Smooth scroll (animation to links in-page)-->
+    <script type="text/javascript" src="js/smooth-scroll.polyfills.min.js"></script>
+    <!-- Prism (displays formatted code boxes) -->
+    <script type="text/javascript" src="js/prism.js"></script>
+    <script type="text/javascript" src="js/zoom.min.js"></script>
+
+    <!-- Required theme scripts (Do not remove) -->
+    <script type="text/javascript" src="js/theme.js"></script>
+
+  </body>
+
+</html>
