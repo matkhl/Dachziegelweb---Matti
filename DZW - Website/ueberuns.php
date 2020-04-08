043c61d071929a28dcb2d5f956d7615e7b150f6b
matkhl <matkhl9323@gmail.com>
2020-04-08 22:03:18 +0200

    Über uns hinzugefügt

---
+++ DZW - Website/ueberuns.php
@@ -0,0 +1,129 @@
+<!--ÜBER UNS-->
+<!DOCTYPE html>
+
+<html lang="de">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Über uns | dzw</title>
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,400i,500" rel="stylesheet">
+    <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/entypo.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+  </head>
+
+  <body class="bg-light">
+
+    <?php include("./imports/header.html"); ?>
+
+    <div class="main-container">
+      <section class="space-lg bg-white">
+        <div class="container">
+          <div class="row align-items-center justify-content-around">
+            <div class="col-md-7 text-left section-intro">
+              <h1 class="display-4">Vom Nachschlagewerk zum Dachziegelweb.</h1>
+              <span class="lead">
+                Stellen Sie sich vor: Der Dachdecker hat das Dach<br>
+                bereits für den bestellten Ziegel eingelattet. Ihr Händler ruft an<br>
+                und verschiebt den Liefertermin um 3 bis 4 Wochen. Der Bauherr<br>
+                verlangt eine schnelle und lieferbare Alternative, damit der<br>
+                vereinbarte Termin für die Dacheindeckung eingehalten werden<br>
+                kann. Im Jahr 2006 gehörten Lieferschwierigkeiten der<br>
+                Hersteller zum ständigen Alltag, für den Bauherren<br>
+                und den Dachdeckern.<br>
+              </span>
+              <a href="#" class="btn btn-success btn-lg">Zur Profisuche</a>
+            </div>
+            <!--end of col-->
+            <div class="col-md-1"></div>
+            <!--end of col-->
+            <div class="col-md-4">
+              <img class="img-fluid avatar d-block w-100 h-100" src="res/img/überuns/angela.jpg"/>
+            </div>
+            <!--end of col-->
+          </div>
+          <!--end of row-->
+        </div>
+      </section>
+
+      <section>
+        <div class="container text-center">
+          <div class="container mb-4" style="max-width: 95%;">
+            <h1>Welcher Ziegel passt?</h1>
+            <p>Zuerst begann es mit der Suche nach einem passenden Ziegel: Decklänge, Form, Farbe und Oberfläche sollten stimmen. Aber nicht Online, sondern in sämtlichen Ordnern mit Produktdatenblättern. Man verschwendete sehr viel Zeit, einen Alternativziegel zu finden, es mußte also eine Lösung her. Daraufhin ist mein erstes Nachschlagewerk, mit seinen 548 Seiten für Sie als Dachprofi, entstanden.</p>
+          </div>
+          <img class="img-fluid rounded" src="res/img/überuns/welcher_ziegel_passt.png"/>
+        </div>
+      </section>
+
+      <section class="bg-white">
+        <div class="container text-left">
+          <h1>Seit 28 Jahren</h1>
+          <p>bin ich jetzt im Handelsbereich für Dachbaustoffe tätig und habe viele Veränderungen seitens der Dachziegel / Dachstein-Industrie miterlebt.<br>
+          In diesem Zeitraum erfolgten Fusionen und Übernahmen von Herstellern. Die Suche und Zuordnungen von Dachziegeln mit dem dazugehörigen Herstellern wurden dadurch aber nicht einfacher. Denn wer es einmal versucht hat, weiß, mit welchem Zeitaufwand es verbunden ist, herauszufinden, welcher Hersteller, welchen Ziegel unter welchen Namen und Farben anbietet. Zum Beispiel erfolgten innerhalb von 3 Jahren (2007-2010): Namensänderungen bei 28 Ziegelmodellen, 49 Dachziegel wurden nicht mehr hergestellt und 28 Modelle neu produziert. In dieser Zeit gab es 245 Farb-, 61 Gewichts- und 37 Maßänderungen.</p>
+        </div>
+      </section>
+
+      <section class="mb-5">
+        <div class="container text-left">
+          <ul class="container">
+            <li class="row mb-5">
+              <div class="col-md-4">
+                <img class="img-fluid rounded" src="res/img/überuns/messe2.jpg">
+              </div>
+              <div class="col-md-8 mt-4">
+                <p>Innerhalb weniger Monate werden immer wieder Namen, Farben und Größen verschiedener Dachziegel geändert, andere werden nicht mehr produziert und es kommen neue Modelle hinzu. Mit einer Printversion konnte man mit den ständigen Veränderungen nicht mehr Schritt halten und so reifte die Idee zu einer digitalen Plattform. Wo, wenn nicht bei der Erstellung und Pflege einer solchen sich ständig ändernden Wissensbasis, ist Digitalisierung sinnvoll und konsequent? Somit wurde die Dachziegelweb entwickelt, eine umfassende Datenbank mit ständig aktualisierten Produktdaten und technischen Informationen, aller relevanten Dachziegel und Dachsteine auf einen Blick.</p>
+              </div>
+            </li>
+            <li class="row mb-5">
+              <div class="col-md-4">
+                <img class="img-fluid rounded" src="res/img/überuns/messe1.jpg">
+              </div>
+              <div class="col-md-8 mt-4">
+                <p>Eine erste positive Resonanz dazu erhielten wir auf der Messe Dach+Holz 2016 in Stuttgart für die Gestaltung, den Aufbau sowie die leichte und einfache Handhabung dieser Website. Durch intensive Gespräche erfuhren wir von den Verarbeitern, Händlern und auch Architekten, wie viel Zeit Sie mit der Suche von Dachziegeln nach bestimmten Auswahlkriterien verbringen. Es wurden hierbei Zeitangaben von monatlich mindestens 3 Stunden genannt je nach Bauvorhabenanzahl. Da dieser hohe Zeitaufwand gleichzeitig ein finanzieller, sowie ein zusätzlicher Kostenfaktor bedeutet, muss der Suchaufwand deutlich verringert werden. Dabei soll unsere digitale Plattform helfen und unterstützen.</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <?php include("./imports/footer.html"); ?>
+
+    </div>
+
+    <script type="text/javascript" src="js/custom.js"></script>
+
+    <!-- Required vendor scripts (Do not remove) -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+
+    <!-- Optional Vendor Scripts (Remove the plugin script here and comment initializer script out of index.js if site does not use that feature) -->
+
+    <!-- AOS (Animate On Scroll - animates elements into view while scrolling down) -->
+    <script type="text/javascript" src="js/aos.js"></script>
+    <!-- Flatpickr (calendar/date/time picker UI) -->
+    <script type="text/javascript" src="js/flatpickr.min.js"></script>
+    <!-- Flickity (handles touch enabled carousels and sliders) -->
+    <script type="text/javascript" src="js/flickity.pkgd.min.js"></script>
+    <!-- jarallax (parallax effect and video backgrounds) -->
+    <script type="text/javascript" src="js/jarallax.min.js"></script>
+    <script type="text/javascript" src="js/jarallax-video.min.js"></script>
+    <script type="text/javascript" src="js/jarallax-element.min.js"></script>
+    <!-- ScrollMonitor (manages events for elements scrolling in and out of view) -->
+    <script type="text/javascript" src="js/scrollMonitor.js"></script>
+    <!-- jQuery smartWizard facilitates steppable wizard content -->
+    <script type="text/javascript" src="js/jquery.smartWizard.min.js"></script>
+    <!-- Smooth scroll (animation to links in-page)-->
+    <script type="text/javascript" src="js/smooth-scroll.polyfills.min.js"></script>
+    <!-- Prism (displays formatted code boxes) -->
+    <script type="text/javascript" src="js/prism.js"></script>
+    <script type="text/javascript" src="js/zoom.min.js"></script>
+
+    <!-- Required theme scripts (Do not remove) -->
+    <script type="text/javascript" src="js/theme.js"></script>
+
+  </body>
+
+</html>
